65ce4f31f557fda79e8726a48f17df1b82f73359
Daniele Belardi <dwon.dnl@gmail.com>
2020-11-07 21:30:53 +0100

    feat: add hrtime2us
    Add conversion to microseconds.

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ exports.hrtime2ns = function (time) {
   return time[0] * 1e9 + time[1]
 }
 
+exports.hrtime2us = function (time) {
+  return time[0] * 1e6 + time[1] / 1e3
+}
+
 exports.hrtime2ms = function (time) {
   return time[0] * 1e3 + time[1] / 1e6
 }
